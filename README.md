501af6ba1676768a190bb6ec6fdf4202f821ec48
hydeki7 <63282607+hydeki7@users.noreply.github.com>
2020-05-15 16:13:45 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# project_1
-my project source code
+# AdminLTE-CRUD-With-Codeigniter
+CRUD dengan Template Admin LTE menggunakan Codeigniter
 
-Terima kasih sudah mendownload
+Fitur : 
+  1. CRUD with Ajax
+  2. Datatable
+  3. Export dan Import ke Excel
+  4. Chart
+  
+Akun Login :
+  1. username : auwfar / password : auwfar
+  2. username : ozil / password : ozil
---
+++ admin.sql
@@ -0,0 +1,262 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 15, 2020 at 11:03 AM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 5.6.36
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `admin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(15) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL,
+  `ket` varchar(222) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`, `nama`, `foto`, `ket`) VALUES
+(1, 'auwfar', 'f0a047143d1da15b630c73f0256d5db0', 'Achmad Chadil Auwfar', 'Koala.jpg', ''),
+(2, 'ozil', 'f4e404c7f815fc68e7ce8e3c2e61e347', 'Mesut ', 'profil2.jpg', ''),
+(3, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'ADMIN', 'profil1.jpg', 'INI ADALAH KETERANGAN');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelamin`
+--
+
+CREATE TABLE `kelamin` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kelamin`
+--
+
+INSERT INTO `kelamin` (`id`, `nama`) VALUES
+(1, 'Laki laki'),
+(2, 'Perempuan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kota`
+--
+
+CREATE TABLE `kota` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kota`
+--
+
+INSERT INTO `kota` (`id`, `nama`) VALUES
+(1, 'Malang'),
+(3, 'Blitar'),
+(4, 'Tulungagung'),
+(17, 'Jakarta'),
+(21, 'Surabaya'),
+(22, 'Paris');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pegawai`
+--
+
+CREATE TABLE `pegawai` (
+  `id` varchar(255) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `telp` varchar(255) DEFAULT NULL,
+  `id_kota` int(11) DEFAULT NULL,
+  `id_kelamin` int(1) DEFAULT NULL,
+  `id_posisi` int(11) DEFAULT NULL,
+  `status` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pegawai`
+--
+
+INSERT INTO `pegawai` (`id`, `nama`, `telp`, `id_kota`, `id_kelamin`, `id_posisi`, `status`) VALUES
+('10', 'Antony Febriansyah Hartono', '082199568540', 1, 1, 1, 1),
+('11', 'Hafizh Asrofil Al Banna', '087859615271', 1, 1, 1, 1),
+('12', 'Faiq Fajrullah', '085736333728', 1, 1, 2, 1),
+('3', 'Mustofa Halim', '081330493322', 1, 1, 3, 1),
+('4', 'Dody Ahmad Kusuma Jaya', '083854520015', 1, 1, 2, 1),
+('5', 'Mokhammad Choirul Ikhsan', '085749535400', 3, 1, 2, 1),
+('7', 'Achmad Chadil Auwfar', '08984119934', 2, 1, 1, 1),
+('8', 'Rizal Ferdian', '087777284179', 1, 1, 3, 1),
+('9', 'Redika Angga Pratama', '083834657395', 1, 1, 3, 1),
+('1', 'Tolkha Hasan', '081233072122', 1, 1, 4, 1),
+('2', 'Wawan Dwi Prasetyo', '085745966707', 4, 1, 4, 1),
+('ea8a7bb40a63d2ae04157a15b108d382', 'Rizki', '08888', 17, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posisi`
+--
+
+CREATE TABLE `posisi` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `posisi`
+--
+
+INSERT INTO `posisi` (`id`, `nama`) VALUES
+(1, 'IT'),
+(2, 'HRD'),
+(3, 'Keuangan'),
+(4, 'Produk'),
+(5, 'Web Developer'),
+(6, 'ACC');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_jenis_perizinan`
+--
+
+CREATE TABLE `tb_jenis_perizinan` (
+  `id_jenis_perizinan` int(3) NOT NULL,
+  `nama_perizinan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_jenis_perizinan`
+--
+
+INSERT INTO `tb_jenis_perizinan` (`id_jenis_perizinan`, `nama_perizinan`) VALUES
+(1, 'KIR'),
+(2, 'SIUP'),
+(3, 'SITU'),
+(4, 'STNK'),
+(5, 'INDIHOME');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id_user` varchar(64) NOT NULL,
+  `nama_user` varchar(64) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `hak_akses` enum('Admin','Admin-bdg','Admin-jkt','Admin-pwk','Admin-psr','bdg','jkt','pwk','psr','head','employee','satpam-bdg','satpam-jkt','satpam-pwk','satpam-psr') NOT NULL,
+  `avatar` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id_user`, `nama_user`, `password`, `hak_akses`, `avatar`) VALUES
+('admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Admin', 'profil2.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kelamin`
+--
+ALTER TABLE `kelamin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kota`
+--
+ALTER TABLE `kota`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posisi`
+--
+ALTER TABLE `posisi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_jenis_perizinan`
+--
+ALTER TABLE `tb_jenis_perizinan`
+  ADD PRIMARY KEY (`id_jenis_perizinan`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `kota`
+--
+ALTER TABLE `kota`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `posisi`
+--
+ALTER TABLE `posisi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tb_jenis_perizinan`
+--
+ALTER TABLE `tb_jenis_perizinan`
+  MODIFY `id_jenis_perizinan` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
